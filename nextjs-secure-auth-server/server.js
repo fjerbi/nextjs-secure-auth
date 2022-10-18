58108537569edcc71c4f02b853c41348dac6035d
fjerbi <firas.jerbi.engineer@gmail.com>
2022-10-18 15:24:02 -0700

    MongoDb config

--- nextjs-secure-auth-server/server.js
+++ nextjs-secure-auth-server/server.js
@@ -1,12 +1,19 @@
 import express from "express";
 import cors from "cors";
 import fs from "fs";
+import mongoose from "mongoose";
 const morgan = require("morgan");
 require("dotenv").config();
 
 // creating express app
 
 const app = express();
+
+// DB Connection
+mongoose
+  .connect(process.env.DATABASE, {})
+  .then(() => console.log("Successfully connected to DB"))
+  .catch((err) => console.log("DB Connection failed", err));
 
 // apply middlewares
 
