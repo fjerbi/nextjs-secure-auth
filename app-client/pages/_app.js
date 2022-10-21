9cddd417f211a6220a7136ed81fb88d03c7bd471
fjerbi <firas.jerbi.engineer@gmail.com>
2022-10-21 05:28:29 -0700

    Csrf and bug fixes

---
+++ app-client/pages/_app.js
@@ -0,0 +1,19 @@
+import TopNav from "../components/TopNav";
+import "../styles/globals.css"
+import "antd/dist/antd.css";
+
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { Provider } from "../context";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <Provider>
+      <ToastContainer position="top-center" />
+      <TopNav />
+      <Component {...pageProps} />
+    </Provider>
+  );
+}
+
+export default MyApp;
---
+++ server/controllers/auth.js
@@ -0,0 +1,81 @@
+import User from "../models/user";
+import { hashPassword, comparePassword } from "../utils/auth";
+import jwt from "jsonwebtoken";
+
+export const register = async (req, res) => {
+  try {
+    // console.log(req.body);
+    const { name, email, password } = req.body;
+    // validation
+    if (!name) return res.status(400).send("Name is required");
+    if (!password || password.length < 6) {
+      return res
+        .status(400)
+        .send("Password is required and should be min 6 characters long");
+    }
+    let userExist = await User.findOne({ email }).exec();
+    if (userExist) return res.status(400).send("Email is taken");
+
+    
+    const hashedPassword = await hashPassword(password);
+
+    const user = new User({
+      name,
+      email,
+      password: hashedPassword,
+    });
+    await user.save();
+
+    return res.json({ ok: true });
+  } catch (err) {
+    console.log(err);
+    return res.status(400).send("Error. Try again.");
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+   
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email }).exec();
+    if (!user) return res.status(400).send("No user found");
+ 
+    const match = await comparePassword(password, user.password);
+
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+   
+    user.password = undefined;
+
+    res.cookie("token", token, {
+      httpOnly: true,
+ 
+    });
+ 
+    res.json(user);
+  } catch (err) {
+    console.log(err);
+    return res.status(400).send("Error. Try again.");
+  }
+};
+
+export const logout = async (req, res) => {
+  try {
+    res.clearCookie("token");
+    return res.json({ message: "Signout success" });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const currentUser = async (req, res) => {
+  try {
+    const user = await User.findById(req._id).select("-password").exec();
+    console.log("CURRENT_USER", user);
+    return res.json({ ok: true });
+  } catch (err) {
+    console.log(err);
+  }
+};
