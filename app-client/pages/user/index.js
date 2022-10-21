6b6164c1bf8a37bcbcd6241a6fb5828543db5bf7
fjerbi <firas.jerbi.engineer@gmail.com>
2022-10-21 06:13:03 -0700

    user profile

--- app-client/pages/user/index.js
+++ app-client/pages/user/index.js
@@ -3,18 +3,18 @@ import { Context } from "../../context";
 import UserRoute from "../../components/routes/UserRoute";
 
 const UserIndex = () => {
+
   const { state, dispatch } = useContext(Context);
   const { user } = state;
 
   return (
     <UserRoute>
-      
       {user !== null && (
         <div>
-          <h1>{user.name}</h1>
-          <h1>{user.email}</h1>
+        <h1>{user.name}</h1>
+        <h1>{user.email}</h1>
         </div>
-      )}
+)}
     </UserRoute>
   );
 };
